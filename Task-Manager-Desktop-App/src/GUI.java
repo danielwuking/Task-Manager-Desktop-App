254ede78f9c75de2830283ac8467e3bd9d197da8
danielwu <danielwu@localhost>
2020-11-10 23:21:19 +0800

    Add the timer function and optimize the table GUI.

--- Task-Manager-Desktop-App/src/GUI.java
+++ Task-Manager-Desktop-App/src/GUI.java
@@ -5,12 +5,8 @@ import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.Vector;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.JTable;
@@ -18,57 +14,55 @@ import javax.swing.JTable;
 
 public class GUI {   
 	private static JFrame frame = new JFrame("Task Manager for Linux");
-	private static JPanel panel = new JPanel();
-    public static void createGUI(InputStream data) {            
-        frame.setSize(100, 100);
+	private static JScrollPane scrollPane = new JScrollPane();
+	private static JTable table = new JTable();
+    public static void createGUI(InputStream data) throws IOException {            
+        frame.setSize(800, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
         placeComponents(data);     
-        frame.add(panel);
         frame.setVisible(true);
     }
     
-    public static void updateGUI(InputStream data) {
-    	panel = new JPanel();
-//    	placeComponents(data);
-    	frame.add(panel);
+    public static void updateGUI(InputStream data) throws IOException {
+    	TableModel model = convertDataToTableModel(data);
+		table.setModel(model);
+    	table.repaint();
+    }
+ 
+    private static void placeComponents(InputStream data) throws IOException {
+		TableModel model = convertDataToTableModel(data);
+		table = new JTable(model);
+		table.setSize(1000, 1000);
+		scrollPane = new JScrollPane(table, 
+				JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, 
+				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS
+				);
+		frame.getContentPane().add(scrollPane);       	    
     }
     
-    
- 
-    private static void placeComponents(InputStream data) {
-        panel.setLayout(null); 
-		try{
-			String dataString =  parseDataToString(data);
-			TableModel model = parseDataStringToTableModel(dataString);
-			JTable table = new JTable(model);
-			table.setSize(10000, 10000);
-			panel.add(table);
-		}catch (IOException exp) {
-		    exp.printStackTrace();
-		}       	    
+    private static TableModel convertDataToTableModel(InputStream data) throws IOException {
+    	String dataString =  parseDataToString(data);
+		TableModel model = parseDataStringToTableModel(dataString);
+		return model;
     }
     
     private static TableModel parseDataStringToTableModel(String dataString) {
     	String dataRows[] = dataString.split("\n");
     	Vector<Vector<String>> dataVector = new Vector<Vector<String>>();
-        for (int i = 12; i < 20; i++) {
+        for (int i = 11; i < dataRows.length; i++) {
         	dataRows[i] = dataRows[i].trim();  //UPDATE
             Vector<String> data = new Vector<String>();
             data.addAll(Arrays.asList(dataRows[i].split("\\s+")));
             dataVector.add(data);
         }
         
-        Vector<String> header = new Vector<String>(2);
-        for (String title : dataRows[11].split("\\s+")) {
+        Vector<String> header = new Vector<String>();
+        for (String title : dataRows[10].split("\\s+")) {
 	        header.add(title);
         }
-//        System.out.println(dataVector.get(0));
+        System.out.println(dataVector.get(0));
         
     	TableModel model = new DefaultTableModel(dataVector, header); 
-    	System.out.println(header);
-    	System.out.println(dataVector.get(0).size());
-    	System.out.println(header.size());
     	return model;
     }
     
--- Task-Manager-Desktop-App/src/index.java
+++ Task-Manager-Desktop-App/src/index.java
@@ -1,17 +1,38 @@
 import java.io.IOException;
 import java.io.InputStream;
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class index {
-	public static void main(String[] args) throws InterruptedException{
+	public static void main(String[] args) throws InterruptedException, IOException{
 		startManager();
-//		while (true) {
-//			Thread.sleep(3000); /* update data per 3sec */
-//			updateManager();
-//		}		
+		updateManagerRegularly();
 	}
 	
+	private static void updateManagerRegularly() {
+		Timer timer = new Timer(0, new ActionListener() {
+			   @Override
+			   public void actionPerformed(ActionEvent e) {
+				   try {
+					updateManager();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+			   }
+			});
+
+			timer.setDelay(3000); // delay for 3 seconds
+			timer.start();
+	}
+	
+	
 	private static InputStream getTaskList() {	
-		ProcessBuilder pb = new ProcessBuilder("top", "-l", "1");
+		String command = 
+				"top -l 1 | grep \" \" | "
+				+ "awk '{ printf(\"%-8s  %-8s  %-8s %-8s %-8s \\n\", "
+				+ "$1, $2, $3, $4, $8. $12, $13); }' ";
+		ProcessBuilder pb = new ProcessBuilder("/bin/sh", "-c", command);
 		try {
 		    Process p = pb.start();
 		    InputStream is = p.getInputStream();
@@ -23,12 +44,12 @@ public class index {
 		return null;
 	}
 	
-	private static void startManager() {
+	private static void startManager() throws IOException {
 		InputStream tasks = getTaskList();
 		GUI.createGUI(tasks);
 	} 
 	
-	private static void updateManager() {
+	private static void updateManager() throws IOException {
 		InputStream tasks = getTaskList();
 		GUI.updateGUI(tasks);		
 	}
